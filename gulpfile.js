b12e9aee83f14d7700274019d05ce3e7f1f33516
Ryan Connelly <ryan@atlantacs.com>
2016-03-10 19:24:54 -0500

    simplified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,60 @@
 const gulp         = require('gulp');
-const path         = require('path');
-const fs           = require('fs');
-const cache        = require('gulp-cached');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps   = require('gulp-sourcemaps');
-const minifyCSS    = require('gulp-minify-css');
-const rename       = require('gulp-rename');
-const concat       = require('gulp-concat');
-const uglify       = require('gulp-uglify');
-const connect      = require('gulp-connect');
-const open         = require('gulp-open');
-const bootlint     = require('gulp-bootlint');
+const browserSync  = require('browser-sync').create();
 const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const s3           = require("gulp-s3");
+const sourcemaps   = require('gulp-sourcemaps');
 const del          = require('del');
-const s3 = require("gulp-s3");
-//const livereload = require('gulp-livereload');
-
-// https://www.npmjs.com/package/gulp-s3
-
+const fs           = require('fs');
+var sequence       = require('run-sequence');
 
 const Paths = {
-  HERE                 : './',
-  ASSETS                : './assets',
-  DEPLOY               :  './public',
-  FILES                 : [
-    '*.html'
-  ]
+  ROOT: './',
+  ASSETS: './assets/',
+  CSS_DIR: './assets/css/',
+  SCSS_DIR: './assets/scss/',
+  SCSS_FILE: './assets/scss/uikit.scss',
+  DEPLOY: 'dist/site',
+  FILES: [
+    'index.html'
+    ]
 };
 
-// Runs a preview
-gulp.task('default', ['preview']);
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: Paths.ROOT
+    });
 
-// Builds the bootstrap theme. Minifies CSS and JS.
-gulp.task('build', ['copy']);
+    gulp.watch(STYLESHEET, ['sass']);
+    gulp.watch(ROOT + '*.html').on('change', browserSync.reload);
+});
 
 gulp.task('clean', function () {
   return del([Paths.DEPLOY]);
 });
 
-gulp.task('watch', function () {
-  gulp.task('sass:watch');
+gulp.task('publish', function(callback) {
+  sequence('clean', 'copy', 'upload', callback);
 });
 
-gulp.task('preview', ['server'], function () {
-  gulp.src(__filename)
-    .pipe(open({uri: 'http://localhost:9001/'}));
+gulp.task('upload', function() {
+  var aws = JSON.parse(fs.readFileSync('./aws.json'));
+  return gulp.src(Paths.DEPLOY + '/**')
+    .pipe(s3(aws));
 });
 
-gulp.task('server', ['sass'], function () {
-  connect.server({
-    root: '',
-    port: 9001,
-    livereload: true
-  });
-});
-
-gulp.task('publish-preview', ['publish-server'], function () {
-  gulp.src(__filename)
-    .pipe(open({uri: 'http://localhost:9001/'}));
-});
-
-gulp.task('publish-server', ['copy'], function () {
-  connect.server({
-    root: 'public',
-    port: 9001,
-    livereload: true
-  });
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(Paths.SCSS_FILE)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ 
+          style: 'expanded',
+          sourceComments: 'normal'
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(Paths.CSS_DIR))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('copy',['copy-assets'], function() {
@@ -73,7 +62,7 @@ gulp.task('copy',['copy-assets'], function() {
     .pipe(gulp.dest(Paths.DEPLOY));
 });
 
-gulp.task('copy-assets', ['clean','sass'], function() {
+gulp.task('copy-assets', ['sass'], function() {
   return gulp.src([Paths.ASSETS + '/js/**/*', 
                    Paths.ASSETS + '/css/**/*', 
                    Paths.ASSETS + '/images/**/*',
@@ -82,36 +71,4 @@ gulp.task('copy-assets', ['clean','sass'], function() {
     .pipe(gulp.dest(Paths.DEPLOY));
 });
 
-gulp.task('publish', ['copy'], function() {
-  var aws = JSON.parse(fs.readFileSync('./aws.json'));
-  return gulp.src(Paths.DEPLOY + '/**')
-    .pipe(s3(aws));
-});
-
-gulp.task('bootlint', function() {
-  return gulp.src(Paths.FILES)
-    .pipe(bootlint({
-      stoponerror: true,
-      stoponwarning: true,
-      loglevel: debug,
-      disabledIds: ['W009', 'E007']
-    }));
-});
-
-gulp.task('sass', function () {
-  return gulp.src(Paths.ASSETS + '/sass/uikit.scss')
-    
-    .pipe(sourcemaps.init())
-    .pipe(sass({ 
-      style: 'expanded',
-      sourceComments: 'normal'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(Paths.ASSETS + '/css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch(Paths.ASSETS + '/sass/**/*.scss', ['sass']);
-});
-
+gulp.task('default', ['serve']);
